7c6cec78152aa8d1e6eb6b3a24a0686a3942964e
michal dub <michal.dub@centrumholdings.com>
2011-03-19 22:01:08 +0100

    added more tests for analysis

--- tests/test_periodic_tasks.py
+++ tests/test_periodic_tasks.py
@@ -26,7 +26,7 @@ def test_getting_correct_query():
     tools.assert_equals((False, ''), check_query(report))
 
 
-def test_create_analysis():
+def test_create_periodic_csv_analysis_for_count():
     report = Report(title="report1", description="test", db_query="db.events.count()", interval='m')
     report.save()
     report_result1 = ReportResult(report=report, output ='{"count" : 5}', run_date = datetime(2010, 5, 6))
@@ -41,6 +41,51 @@ def test_create_analysis():
     tools.assert_not_equals(Analysis.objects.all()[0].last_report, None)
     analysis_result1 = AnalysisResult.objects.all()[0]
     os.remove('%s/%s' % (STATISTICS_REPORT_PATH, analysis_result1.output))
+
+
+def test_create_aperiodic_graph_analysis_for_count():
+    report = Report(title="report2", description="test", db_query='db.events.find({event: "statistc"}).count()', interval='w')
+    report.save()
+    report_result1 = ReportResult(report=report, output ='{"count" : 15}', run_date = datetime(2010, 5, 13))
+    report_result1.save()
+    report_result2 = ReportResult(report=report, output ='{"count" : 97}', run_date = datetime(2010, 5, 6))
+    report_result2.save()
+    analysis = Analysis(id=2, title="analysis2", description="test analysis", plug_in='output_bar_graph_file', interval='n')
+    analysis.queries.add(report)
+    analysis.save()
+    tools.assert_equals(True, create_analysis())
+    tools.assert_equals(AnalysisResult.objects.all().count(), 1)
+    tools.assert_not_equals(Analysis.objects.all()[0].last_report, None)
+
+
+def test_create_periodic_graph_analysis_for_group():
+    report = Report(title="report3", description="test", db_query='db.events.group({ key : { event : true }, condition : { timestamp : {$gt : ${{d1}}, $lt: ${{d2}}} }, reduce : function( obj, prev ) { prev.total++; }, initial : { total : 0 } })', interval='y')
+    report.save()
+    report_result1 = ReportResult(report=report, output ='{ "event" : "main_event7_1", "total" : 4 }\n{ "event" : "main_event6_1", "total" : 4 }\n', run_date = datetime(2011, 5, 13))
+    report_result1.save()
+    report_result2 = ReportResult(report=report, output ='{ "event" : "main_event7_1", "total" : 6 }\n{ "event" : "main_event5_1", "total" : 12 }\n', run_date = datetime(2010, 5, 13))
+    report_result2.save()
+    analysis = Analysis(id=3, title="analysis3", description="test analysis", plug_in='output_bar_graph_file', interval='y')
+    analysis.queries.add(report)
+    analysis.save()
+    tools.assert_equals(True, create_analysis())
+    tools.assert_equals(AnalysisResult.objects.all().count(), 1)
+    tools.assert_not_equals(Analysis.objects.all()[0].last_report, None)
+
+
+def test_create_periodic_csv_analysis_for_group():
+    report = Report(title="report4", description="test", db_query='db.events.group({ key : { event : true }, condition : { timestamp : {$gt : ${{d1}}, $lt: ${{d2}}} }, reduce : function( obj, prev ) { prev.total++; }, initial : { total : 0 } })', interval='m')
+    report.save()
+    report_result1 = ReportResult(report=report, output ='{ "event" : "services_list", "total" : 13 }\n{ "event" : "main_event6_1", "total" : 4 }\n', run_date = datetime(2011, 6, 13))
+    report_result1.save()
+    report_result2 = ReportResult(report=report, output ='{ "event" : "main_event7_1", "total" : 60 }\n{ "event" : "content_link", "total" : 20 }\n', run_date = datetime(2010, 5, 13))
+    report_result2.save()
+    analysis = Analysis(id=4, title="analysis4", description="test analysis", plug_in='output_csv_file', interval='y')
+    analysis.queries.add(report)
+    analysis.save()
+    tools.assert_equals(True, create_analysis())
+    tools.assert_equals(AnalysisResult.objects.all().count(), 1)
+    tools.assert_not_equals(Analysis.objects.all()[0].last_report, None)
 
 
 
