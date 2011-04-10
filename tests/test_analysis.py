6c32235fbec8153ef764768aceb3df4a83e86812
michal dub <michal.dub@centrumholdings.com>
2011-04-10 22:40:00 +0200

    added new tests for testing analyses with mapreduce querey

--- tests/test_analysis.py
+++ tests/test_analysis.py
@@ -68,7 +68,33 @@ def test_create_periodic_csv_analysis_for_group():
     tools.assert_equals(True, create_analysis())
     tools.assert_equals(AnalysisResult.objects.all().count(), 1)
     tools.assert_not_equals(Analysis.objects.all()[0].last_report, None)
+    
+def test_create_periodic_csv_analysis_for_mapreduce():
+    report = Report(title="report5", description="test", db_query='m = function() { if (this.event == "statistic") { emit(this.params.url, 1); }};r = function(url, nums) { var total=0; for (var i=0; i<nums.length; i++) { total += nums[i]; } return total; };res = db.events.mapReduce(m, r);res.find().forEach(printjson);', interval='m')
+    report.save()
+    report_result1 = ReportResult(report=report, output ='{ "_id" : "www.centrum.cz", "value" : 100 }\n{ "_id" : "www.amapy.cz", "value" : 41 }\n', run_date = datetime(2011, 6, 13))
+    report_result1.save()
+    report_result2 = ReportResult(report=report, output ='{ "_id" : "www.atlas.cz", "value" : 6 }\n{ "_id" : "www.cetrum.cz/email/", "value" : 29 }\n', run_date = datetime(2011, 5, 13))
+    report_result2.save()
+    analysis = Analysis(id=5, title="analysis5", description="test analysis", plug_in='output_csv_file', interval='m')
+    analysis.queries.add(report)
+    analysis.save()
+    tools.assert_equals(True, create_analysis())
+    tools.assert_equals(AnalysisResult.objects.all().count(), 1)
+    tools.assert_not_equals(Analysis.objects.all()[0].last_report, None)
 
 
-
+def test_create_periodic_graph_analysis_for_mapreduce():
+    report = Report(title="report6", description="test", db_query='m = function() { if (this.event == "statistic") { emit(this.params.url, 1); }};r = function(url, nums) { var total=0; for (var i=0; i<nums.length; i++) { total += nums[i]; } return total; };res = db.events.mapReduce(m, r);res.find().forEach(printjson);', interval='m')
+    report.save()
+    report_result1 = ReportResult(report=report, output ='{ "_id" : "www.centrum.cz", "value" : 100 }\n{ "_id" : "www.amapy.cz", "value" : 41 }\n', run_date = datetime(2011, 6, 13))
+    report_result1.save()
+    report_result2 = ReportResult(report=report, output ='{ "_id" : "www.atlas.cz", "value" : 6 }\n{ "_id" : "www.centrum.cz", "value" : 29 }\n', run_date = datetime(2011, 6, 12))
+    report_result2.save()
+    analysis = Analysis(id=6, title="analysis6", description="test analysis", plug_in='output_bar_graph_file', interval='d')
+    analysis.queries.add(report)
+    analysis.save()
+    tools.assert_equals(True, create_analysis())
+    tools.assert_equals(AnalysisResult.objects.all().count(), 1)
+    tools.assert_not_equals(Analysis.objects.all()[0].last_report, None)
 
